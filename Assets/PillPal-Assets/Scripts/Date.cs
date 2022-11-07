5672dfc890043c846c9f0d14d8587fc578d7b218
JustinMacGregor <justinmacgregor4@gmail.com>
2022-11-07 14:59:52 -0500

    Template screens created

---
+++ Assets/PillPal-Assets/Scripts/Date.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Date : MonoBehaviour
+{
+
+    public TextMeshPro largeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string time = System.DateTime.UtcNow.ToLocalTime().ToString("d/M/yyyy hh:mmtt");
+        print(time);
+        largeText.text = "Date and Time: " + time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
