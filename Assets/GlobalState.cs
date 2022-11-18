bca136199a503daf0d896c6a8d8abdb48f097ba2
JustinMacGregor <justinmacgregor4@gmail.com>
2022-11-18 14:19:12 -0500

    Global states

---
+++ Assets/GlobalState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalState : MonoBehaviour
+{
+    public int numPills;
+    public static GlobalState instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void incNumPills(int numPills)
+    {
+        numPills += 1;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PillPanelLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PillPanelLogic : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro pillText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void updateText(int amount)
+    {
+        pillText.text = GlobalState.instance.numPills.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
