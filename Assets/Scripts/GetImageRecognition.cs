47bc10ee3ad99079effc5065b10ab45ab4f4b3e2
JustinMacGregor <justinmacgregor4@gmail.com>
2022-11-19 13:09:49 -0500

    Clarifai API added

---
+++ Assets/Scripts/GetImageRecognition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Clarifai.API;
+using Clarifai.DTOs.Inputs;
+using Clarifai.DTOs.Predictions;
+
+public class ImageRecognitionCall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
