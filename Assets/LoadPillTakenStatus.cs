daabaa13749a51b51b2a6fb851956085ed27714e
Alex Theil <alex.theil@gmail.com>
2022-12-07 20:19:23 -0500

    Pills status text mesh now updates to either Taken or Not Taken depending on the PlayerPrefs value "pillsTakenTodayBool"

---
+++ Assets/LoadPillTakenStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LoadPillTakenStatus : MonoBehaviour
+{
+    public int pillsTakenTodayInt;
+
+    [SerializeField] private TextMeshPro pillsTakenText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pillsTakenTodayInt = PlayerPrefs.GetInt("pillsTakenTodayBool");
+        if (pillsTakenTodayInt == 0)
+        {
+            pillsTakenText.text = "Pills Status: Not Taken";
+        }
+        else
+        {
+            pillsTakenText.text = "Pills Status: Taken";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
